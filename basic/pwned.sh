8f2f05614614f494004622874ec1cb5379618092
Ion Tataru <ion@snapiot.com>
2020-09-26 13:39:11 +0200

    clean extra space

--- basic/pwned.sh
+++ basic/pwned.sh
@@ -2,7 +2,7 @@
 # FAIR License, Copyright (c) 2019 72Zn
 # Usage of the works is permitted provided that this instrument is retained
 # with the works, so that any entity that uses the works is notified of this
-# instrument.  
+# instrument.
 # DISCLAIMER: THE WORKS ARE WITHOUT WARRANTY.
 
 # usage examples:
